dfad353076314f560b8ffa80487cc83bdada8d6e
JUNE <63999666+DevelopJun@users.noreply.github.com>
2021-07-30 09:26:44 +0900

    Rename Eventhandler.js to javascript(basic)/Eventhandler.js

--- Eventhandler.js
+++
@@ -1,4 +0,0 @@
-function btnClick(event) {
-	alert(event.target.value+"클릭됨!!");
-}
-document.getElementById('b2').onclick = function(event) {alert(event.target.value+"클릭됨!!")}
---
+++ javascript(basic)/Eventhandler.js
@@ -0,0 +1,4 @@
+function btnClick(event) {
+	alert(event.target.value+"클릭됨!!");
+}
+document.getElementById('b2').onclick = function(event) {alert(event.target.value+"클릭됨!!")}
